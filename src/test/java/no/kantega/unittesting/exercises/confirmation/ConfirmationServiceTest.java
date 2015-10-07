37340bab950d9427fed51b7fe30519f4376c586c
Frode Standal <frode.standal@kantega.no>
2015-10-07 11:10:50 +0200

    solutions to task 10

--- src/test/java/no/kantega/unittesting/exercises/confirmation/ConfirmationServiceTest.java
+++ src/test/java/no/kantega/unittesting/exercises/confirmation/ConfirmationServiceTest.java
@@ -20,6 +20,15 @@ public class ConfirmationServiceTest {
     private RoleRepository roleRepository;
     private ConfirmationRepository confirmationRepository;
 
+    private Document document;
+    private Role pilot;
+    private Role host;
+    private DistributionGroup onBoardPersonnel;
+    private Employee aPilot;
+    private Employee aHost;
+    private List<Employee> employeesInDistributionGroup;
+
+
     @Before
     public void setup() {
 
@@ -27,40 +36,105 @@ public class ConfirmationServiceTest {
         confirmationRepository = mock(ConfirmationRepository.class);
         confirmationService = new ConfirmationService(roleRepository, confirmationRepository);
 
+        document = new Document(1L);
+        aPilot = new Employee(1L);
+        aHost = new Employee(2L);
+        pilot = new Role("PILOT");
+        host = new Role("HOST");
+        onBoardPersonnel = new DistributionGroup();
+
+    }
+
+    @Test
+    public void shallPersistNewConfirmations() {
+
+        givenDocumentToBeDistributedToEmployees();
+
+        whenRequiringConfirmationForDocument();
+
+        thenConfirmationsArePersisted();
+
     }
 
     @Test
     public void shallCreatePendingConfirmations() {
 
-        Role pilot = new Role("PILOT");
-        Role host = new Role("HOST");
-        DistributionGroup onBoardPersonnel = new DistributionGroup();
-        given(roleRepository.findRolesForDistributionGroup(onBoardPersonnel)).willReturn(Arrays.asList(pilot, host));
+        givenDocumentToBeDistributedToEmployees();
 
-        Employee aPilot = new Employee(1L);
-        Employee aHost = new Employee(2L);
+        whenRequiringConfirmationForDocument();
+
+        thenConfirmationsArePending();
+
+    }
+
+    @Test
+    public void shallCreateConfirmationsForDocument() {
+
+        givenDocumentToBeDistributedToEmployees();
+
+        whenRequiringConfirmationForDocument();
+
+        thenConfirmationsApplyToDistributedDocument();
+
+    }
+
+    @Test
+    public void shallCreateConfirmationsForAllEmployeesInDistributionGroup() {
+
+        givenDocumentToBeDistributedToEmployees();
+
+        whenRequiringConfirmationForDocument();
+
+        thenConfirmationsApplyToAllEmployeesInDistributionGroup();
+
+    }
+
+    private void givenDocumentToBeDistributedToEmployees() {
+
+        document.setDistributionGroup(onBoardPersonnel);
+        employeesInDistributionGroup = Arrays.asList(aPilot, aHost);
+
+        given(roleRepository.findRolesForDistributionGroup(onBoardPersonnel)).willReturn(Arrays.asList(pilot, host));
         given(roleRepository.findEmployeesWithRole(pilot)).willReturn(Arrays.asList(aPilot));
         given(roleRepository.findEmployeesWithRole(host)).willReturn(Arrays.asList(aHost));
-
-        Document document = new Document(1L);
-        document.setDistributionGroup(onBoardPersonnel);
-
         given(confirmationRepository.find(any(Employee.class), same(document))).willReturn(null);
 
+    }
+
+    private void whenRequiringConfirmationForDocument() {
         confirmationService.requireConfirmationForDistributedDocuments(Arrays.asList(document));
+    }
 
-        List<Employee> employeesInDistributionGroup = Arrays.asList(aPilot, aHost);
-        ArgumentCaptor<Confirmation> argument = ArgumentCaptor.forClass(Confirmation.class);
-        verify(confirmationRepository, times(employeesInDistributionGroup.size())).insert(argument.capture());
+    private void thenConfirmationsArePersisted() {
+        ArgumentCaptor<Confirmation> captor = ArgumentCaptor.forClass(Confirmation.class);
+        verify(confirmationRepository, times(employeesInDistributionGroup.size())).insert(captor.capture());
+    }
 
-        List<Confirmation> confirmations = argument.getAllValues();
-        List<Employee> actual = new ArrayList<>();
-        for (Confirmation confirmation : confirmations) {
+    private void thenConfirmationsArePending() {
+        for (Confirmation confirmation : capturedConfirmations()) {
             assertTrue(confirmation.isPending());
+        }
+    }
+
+    private void thenConfirmationsApplyToDistributedDocument() {
+        for (Confirmation confirmation : capturedConfirmations()) {
             assertEquals(document, confirmation.getConfirmedDocument());
+        }
+
+    }
+
+    private void thenConfirmationsApplyToAllEmployeesInDistributionGroup() {
+        List<Employee> actual = new ArrayList<>();
+        for (Confirmation confirmation : capturedConfirmations()) {
             actual.add(confirmation.getConfirmedBy());
         }
         assertEquals(Arrays.asList(aPilot, aHost), actual);
+    }
+
+    private List<Confirmation> capturedConfirmations() {
+        ArgumentCaptor<Confirmation> captor = ArgumentCaptor.forClass(Confirmation.class);
+        verify(confirmationRepository, times(employeesInDistributionGroup.size())).insert(captor.capture());
+        return captor.getAllValues();
     }
 
 }
