522432be25c6f3df7b6b0ba2a235eeeb153ce337
Frode Standal <frode.standal@kantega.no>
2015-10-07 10:39:34 +0200

    solutions to task 8

--- src/test/java/no/kantega/unittesting/exercises/generator/IdGeneratorTest.java
+++ src/test/java/no/kantega/unittesting/exercises/generator/IdGeneratorTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,6 +18,7 @@ public class IdGeneratorTest {
 
     private final int threads = 100;
     private final int callsPerThread = 100;
+    private final CountDownLatch allThreadsComplete = new CountDownLatch(threads);
     private IdGenerator generator;
 
     @Before
@@ -32,13 +35,14 @@ public class IdGeneratorTest {
             for (int i = 0; i < callsPerThread; i++) {
                 uniqueValues.add(generator.getUniqueId());
             }
+            allThreadsComplete.countDown();
         };
 
         for (int i = 0; i < threads; i++) {
             new Thread(runnable).start();
         }
 
-        Thread.sleep(500);
+        allThreadsComplete.await(10, TimeUnit.SECONDS);
 
         int expectedNoOfValues = threads * callsPerThread;
         assertEquals(expectedNoOfValues, uniqueValues.size());
