45faf0c4c1523e20b3cefd8a627910e50571689d
Frode Standal <frode.standal@kantega.no>
2015-10-07 10:22:28 +0200

    solutions to task 4

--- src/main/webapp/spec/DateUtil.spec.js
+++ src/main/webapp/spec/DateUtil.spec.js
@@ -1,26 +1,31 @@
 describe("dateUtil", function() {
 
+    var second = 1000; //in milliseconds
+    var minute = 60 * second;
+    var hour = 60 * minute;
+    var day = 24 * hour;
+    var week = 7 * day;
+
     it("shall display 8 day difference as '1 week ago'", function () {
         var dateUtil = new DateUtil();
         var now = new Date();
-        var date = new Date(now - 8 * 24 * 60 * 60 * 1000);
-        expect(dateUtil.durationAsText(date, now)).toBe("1 week ago");
+        var date8DaysAgo = new Date(now - 8 * day);
+        expect(dateUtil.durationAsText(date8DaysAgo, now)).toBe("1 week ago");
     });
 
     it("shall display difference with days, hours, minutes and seconds", function () {
         var dateUtil = new DateUtil();
         var now = new Date();
-        var date = new Date(now - (3 * 24 * 60 * 60 * 1000) - (2 * 60 * 60 * 1000) - (16 * 60 * 1000) - (10 * 1000));
+        var date = new Date(now - (3 * day + 2 * hour + 16 * minute + 10 * second));
         expect(dateUtil.durationAsText(date, now)).toBe("3 days, 2 hours, 16 minutes and 10 seconds ago");
     });
 
     it("shall display differences into the future", function () {
         var dateUtil = new DateUtil();
         var now = new Date();
-        var date = new Date(now.getTime() + 24 * 60 * 60 * 1000);
+        var dateOneDayFromNow = new Date(now.getTime() + 1 * day);
 
-        expect(dateUtil.durationAsText(date, now)).toBe("in 24 hours");
+        expect(dateUtil.durationAsText(dateOneDayFromNow, now)).toBe("in 24 hours");
     });
-
 });
 
